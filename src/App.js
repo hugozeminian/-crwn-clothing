c484734f9c9975d58529801e576b8e5bda8c1c9a
Hugo Zeminian <hugozamian@hotmail.com>
2023-03-16 17:57:06 -0600

    76. Scaffolding Our Capstone Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 const App = () => {
   return (
-    <div>
-      Hello world!
+    <div className="categories-container">
+      <div className="category-container">
+        {/*<img />*/}
+        <div className="category-body-container">
+          <h2>Hates</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   )
 }
