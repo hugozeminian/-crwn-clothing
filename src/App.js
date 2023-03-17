216c2ef5daff4a7c3161ecf5be73c3cafc2911f7
Hugo Zeminian <hugozamian@hotmail.com>
2023-03-17 09:46:49 -0600

    87. React Router Link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./routes/navigation/navigation.component";
 
 import Home from "./routes/home/home.component";
 //import Navigation from './routes/navigation/navigation.component';
 //import SignIn from './routes/sign-in/sign-in.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
